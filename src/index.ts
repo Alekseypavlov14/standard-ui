aad29daf043ca8b5b64519fdef3857dbc7893b6b
Alekseypavlov14 <aleshapavlov9@gmail.com>
2022-12-28 12:24:26 +0200

    added new component to export list

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { BurgerButton } from './components/BurgerButton/BurgerButton'
 import { Button } from './components/Button/Button'
 import { Confirm } from './components/Confirm/Confirm'
+import { Container } from './components/Container/Container'
 import { Date } from './components/Date/Date'
 import { Modal } from './components/Modal/Modal'
+import { Title } from './components/Title/Title'
 
-export { BurgerButton, Button, Confirm, Date, Modal }
+export { BurgerButton, Button, Confirm, Container, Date, Modal, Title }
