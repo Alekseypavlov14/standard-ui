50ae171b245aa42d9e6b617080d345a469e7fe28
Alekseypavlov14 <aleshapavlov9@gmail.com>
2022-12-28 11:59:46 +0200

    added Container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactNode } from 'react'
+import { CustomizationProvider } from '../../features/CustomizationProvider/CustomizationProvider'
+import cn from 'classnames'
+import  './Container.css'
+
+interface ContainerProps extends CustomizationProvider {
+  children: ReactNode
+}
+
+export const Container: FC<ContainerProps> = ({ children, className }) => {
+  return (
+    <div className={cn('__Container__', className)}>
+      {children}
+    </div>
+  )
+}
